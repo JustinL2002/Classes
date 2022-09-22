16ca551873129de99bc62378b6093961aa9312cf
Justin Lawson <jmlawson2002@gmail.com>
2022-09-22 11:40:53 -0400

    classes excercise

---
+++ Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using Classes;
+
+Car myCar = new Car();
+
+    myCar.Make = "Tesla";
+    myCar.Model = "S";
+    myCar.Year = 2020;
+
+    Console.WriteLine(myCar.Make);
+    Console.WriteLine(myCar.Model);
+    Console.WriteLine(myCar.Year);
+
